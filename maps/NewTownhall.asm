7a5fce56ef4cd42039e0cb3364eb40e4c2e3960b
New <naj690@yahoo.com>
2025-10-16 23:05:38 +0800

    Corrected tile behaviour for bridge in forest

--- maps/NewTownhall.asm
+++ maps/NewTownhall.asm
@@ -11,3 +11,14 @@ NewTownhall_MapScriptHeader:
     def_bg_events
 
     def_object_events
+    object_event 33, 15, SPRITE_CAMPER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, NewTownhallHMScript, -1
+
+NewTownhallHMScript:
+    showtext Text_HMEnabler
+    end
+
+Text_HMEnabler:
+    text "Hello, <PLAYER>."
+    para "This is to test"
+    line "the #mon text!"
+    done
